2e7a5296c0c2dfb5e7453c1640d9473a9442456b
Hyoyoung Lim <hyoyounglim@Hyoyoungui-MacBookPro.local>
2025-01-09 22:53:42 +0900

    add debugging msg and save it into a txt file

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 echo "Starting ECN web-server measurement using input file : filelist_server.txt";
 mkdir ecnserver
-fx=`cat filelist_server.txt`
-for filename in $fx
-do
-echo "$filename"
-fx2=`cat $filename`
-for filename_2 in $fx2
-do
-fx3=`echo $filename_2 | cut -f2 -d ','`
-echo "$fx3"
-python3 ecn.py $fx3
-pkill -9 python3
-done
-done
+# fx=`cat filelist_server.txt`
+# for filename in $fx
+# do
+# echo "$filename"
+# fx2=`cat $filename`
+# for filename_2 in $fx2
+# do
+# fx3=`echo $filename_2 | cut -f2 -d ','`
+# echo "$fx3"
+# python3 ecn.py $fx3
+# pkill -9 python3
+# done
+# done
 
 fx=`cat filelist_server.txt`
 for filename in $fx
